e31b4bb0a836cda91803464dde25b96367145eba
Pranav-P26 <pramod.pranav26@gmail.com>
2025-08-01 20:41:29 -0400

    Created a function to deserialize JSON data

--- src/dione.rs
+++ src/dione.rs
@@ -1,10 +1,26 @@
 use crate::module::Module;
+use serde::Deserialize;
+use serde_json;
+use std::fs;
 
 pub struct Dione;
+
+// Fields in this struct will change based on json format
+#[derive(Debug, Deserialize)]
+pub struct Task {
+    pub id: String,
+    pub description: String,
+    pub priority: u8,
+}
 
 impl Dione {
     pub fn new() -> Self {
         Dione
+    }
+
+    pub fn load_json(path: &str) -> Vec<Task> {
+        let data = fs::read_to_string(path).expect("Failed to read JSON file");
+        serde_json::from_str(&data).expect("Failed to parse JSON")
     }
 }
 
@@ -23,6 +39,13 @@ impl Module for Dione {
 
     fn run(&mut self) {
         println!("[{}] run", self.name());
+
+        //Sample file path; must be changed later
+        let tasks = Dione::load_json("tasks.json");
+        println!("Loaded {} tasks", tasks.len());
+        for task in tasks {
+            println!("{:?}", task);
+        }
     }
 
     fn stop(&mut self) {
